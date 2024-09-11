2f10cee928ff1b921d72ba3b832b263122b455b2
Artem <109523883+ArtemNikit1n@users.noreply.github.com>
2024-09-11 20:47:43 +0300

    NullElements

--- Homework06.09.24.c
+++ Homework06.09.24.c
@@ -1,23 +1,25 @@
 ﻿#include <stdio.h>
 
-#define _CRT_SECURE_NO_WARNINGS
-
 
 void NullElements()
 {
-    printf("%s\n", "Enter the numbers of the array");
+    printf("%s\n", "Enter the array size (<= 100):");
 
-    int arrayNumbers[6];
-    int lengthArrayNumbers = sizeof(arrayNumbers) / sizeof(arrayNumbers[0]);
-    int nullCounter = 0;
-    int additionIndex = 0;
+    const int lengthArrayNumbers;
+    scanf_s("%d", &lengthArrayNumbers);
 
-    for (int i = 0; i < 5; ++i) {
-        arrayNumbers[additionIndex] = scanf_s("%d", &arrayNumbers[additionIndex]);
+    if (lengthArrayNumbers > 100) {
+        printf("%s\n", "The array size exceeds the allowed limit.");
+        return 1;
     }
 
-    for (int i = 0; i < lengthArrayNumbers; i++)
-    {
+    printf("%s\n", "Enter the numbers of the array:");
+
+    int arrayNumbers[100];
+    int nullCounter = 0;
+
+    for (int i = 0; i < lengthArrayNumbers; ++i) {
+        scanf_s("%d", &arrayNumbers[i]);
         if (arrayNumbers[i] == 0) nullCounter += 1;
     }
 
@@ -28,7 +30,7 @@ void NullElements()
 int main(void)
 {
     printf("%s\n", "Enter task number:");
-    int taskNumber = scanf_s("%d", &taskNumber);
+    const int taskNumber = scanf_s("%d", &taskNumber);
 
     if (taskNumber == 1)  NullElements();
  
